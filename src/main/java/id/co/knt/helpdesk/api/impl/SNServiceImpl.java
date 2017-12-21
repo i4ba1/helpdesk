87815912956497b1d8acbc6a8430dd48361a30b7
MNI <muhamad.iqbal1987@hotmail.com>
2017-12-21 14:07:05 +0700

    add calculation for total activation number

--- src/main/java/id/co/knt/helpdesk/api/impl/SNServiceImpl.java
+++ src/main/java/id/co/knt/helpdesk/api/impl/SNServiceImpl.java
@@ -200,13 +200,15 @@ public class SNServiceImpl implements SNService {
 					return map;
 				}
 
+				int increaseActivation = (int)serialNumber.getNumberOfActivation() + 1;
+				short totalActivation = (short)increaseActivation;
 				if (license == null) {
 					serialNumber.setActivationKey(activationKey);
-					serialNumber.setNumberOfActivation((short) (serialNumber.getNumberOfActivation() + 1));
+					serialNumber.setNumberOfActivation(totalActivation);
 					license = saveLicenseData(serialNumber, product);
 					map.put("license", license);
 				} else {
-					license.setNumberOfActivation((short) (serialNumber.getNumberOfActivation() + 1));
+					license.setNumberOfActivation(totalActivation);
 					license.setActivationKey(activationKey);
 					license.setPassKey(serialNumber.getPassKey());
 					license.setSchoolName(serialNumber.getSchoolName());
