0b3fe2ac57c6301b8b19a7ce09087c2e62b75c82
root <muhamad.iqbal1987@gmail.com>
2018-04-18 11:40:04 +0700

    Change the fixedRate to=1800000 Change db to helpdesk_test

--- src/main/java/id/co/knt/helpdesk/api/component/SessionComponent.java
+++ src/main/java/id/co/knt/helpdesk/api/component/SessionComponent.java
@@ -16,7 +16,7 @@ public class SessionComponent {
         this.loginRepository = loginRepository;
     }
 
-    @Scheduled(fixedRate = 900000)
+    @Scheduled(fixedRate = 1800000)
     public void kickOutActiveUser(){
         Iterable<Login> logins = loginRepository.findAll();
         if (logins != null) {
