948cb685c6895a5fc433af22c19bcea71e1ec490
root <muhamad.iqbal1987@gmail.com>
2018-04-18 11:36:31 +0700

    Add session component to remove user in login table in every 15 minutes

---
+++ src/main/java/id/co/knt/helpdesk/api/component/SessionComponent.java
@@ -0,0 +1,26 @@
+package id.co.knt.helpdesk.api.component;
+
+import id.co.knt.helpdesk.api.model.Login;
+import id.co.knt.helpdesk.api.repositories.LoginRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionComponent {
+
+    private final LoginRepository loginRepository;
+
+    @Autowired
+    public SessionComponent(LoginRepository loginRepository) {
+        this.loginRepository = loginRepository;
+    }
+
+    @Scheduled(fixedRate = 900000)
+    public void kickOutActiveUser(){
+        Iterable<Login> logins = loginRepository.findAll();
+        if (logins != null) {
+            loginRepository.deleteAll();
+        }
+    }
+}
